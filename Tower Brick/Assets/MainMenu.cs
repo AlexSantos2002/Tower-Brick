67342d68e973b2497b5125d565e8b674e0b48699
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2025-03-29 17:59:57 +0000

    Game Menu UI
    Sprites dos botões e fundo do menu, Implementação dos botões e implementação dos metodos para o botão Play e para o botão Quit

---
+++ Tower Brick/Assets/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame(){
+        SceneManager.LoadSceneAsync("GameScene");
+    }
+
+    public void QuitGame(){
+        Application.Quit();
+        Debug.Log("O jogo foi fechado.");
+    }
+}
