bc30fab9107b6cd13b336b7932fa376a93580844
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2025-03-30 14:59:34 +0100

    Menu buttons sound

--- Tower Brick/Assets/Scripts/MainMenu.cs
+++ Tower Brick/Assets/Scripts/MainMenu.cs
@@ -1,26 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame(){
-        SceneLoader.LoadScene("GameScene");
+    public void PlayGame()
+    {
+        StartCoroutine(DelayedLoad("GameScene"));
     }
 
-    public void Options(){
-        SceneLoader.LoadScene("Options");
+    public void Options()
+    {
+        StartCoroutine(DelayedLoad("Options"));
     }
 
-    public void ToDoWarning(){
-        SceneLoader.LoadScene("ToDoWarning");
+    public void ToDoWarning()
+    {
+        StartCoroutine(DelayedLoad("ToDoWarning"));
     }
 
-    public void Back(){
-        SceneLoader.LoadLastScene();
+    public void Back()
+    {
+        StartCoroutine(DelayedBack());
     }
 
-    public void QuitGame(){
+    public void QuitGame()
+    {
+        // Sem delay porque Application.Quit precisa ser imediato
         Application.Quit();
         Debug.Log("O jogo foi fechado.");
+    }
+
+    private IEnumerator DelayedLoad(string sceneName)
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneLoader.LoadScene(sceneName);
+    }
+
+    private IEnumerator DelayedBack()
+    {
+        yield return new WaitForSeconds(0.5f);
+        SceneLoader.LoadLastScene();
     }
 }
