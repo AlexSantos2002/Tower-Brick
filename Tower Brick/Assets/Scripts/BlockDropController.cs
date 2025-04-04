d6af97281fa8e624a616c4571c2c1c967617ac33
AMTS18 <64600055+AlexSantos2002@users.noreply.github.com>
2025-04-04 21:04:35 +0100

    Drop Evaluation 1.0
    Quando o bloco cai vai dar uma avaliação (Perfect, Bom, Ok), agr que já está implementado apenas teremos que modificar para que apareça uma sprite em vez de um texto simples e melhorar a posição onde aparece a avaliação

--- Tower Brick/Assets/Scripts/BlockDropController.cs
+++ Tower Brick/Assets/Scripts/BlockDropController.cs
@@ -35,7 +35,6 @@ public class BlockDropController : MonoBehaviour
         if (isDropped && !hasLanded && transform.position.y < cameraBottomY - bottomLimitOffset)
         {
             Debug.Log("Bloco falhou e foi destruído.");
-
             gameOverManager?.TriggerGameOver();
             Destroy(gameObject);
         }
@@ -76,6 +75,54 @@ public class BlockDropController : MonoBehaviour
                 Debug.Log("Bloco pousado com sucesso.");
 
                 SFXManager.Instance?.PlayBlockLandingSound();
+
+                string message = "";
+                Color color = Color.white;
+
+                if (overlapPercentage >= 0.9f)
+                {
+                    message = "PERFECT";
+                    color = Color.blue;
+                }
+                else if (overlapPercentage >= 0.7f)
+                {
+                    message = "Bom";
+                    color = Color.green;
+                }
+                else if (overlapPercentage >= 0.5f)
+                {
+                    message = "Ok!";
+                    color = Color.yellow;
+                }
+
+                if (!string.IsNullOrEmpty(message))
+                {
+                    GameObject textObj = new GameObject("LandingFeedbackText");
+
+                    TextMesh textMesh = textObj.AddComponent<TextMesh>();
+                    textMesh.text = message;
+                    textMesh.color = color;
+                    textMesh.fontSize = 30;
+                    textMesh.characterSize = 0.2f;
+                    textMesh.fontStyle = FontStyle.Bold;
+                    textMesh.anchor = TextAnchor.LowerRight;
+                    textMesh.alignment = TextAlignment.Right;
+
+                    Bounds bounds = GetComponent<SpriteRenderer>().bounds;
+                    float marginX = 0.7f;
+                    float marginY = 0.2f;
+                    Vector3 offset = new Vector3(bounds.extents.x + marginX, -bounds.extents.y - marginY, -0.1f);
+                    textObj.transform.position = transform.position + offset;
+
+                    MeshRenderer renderer = textObj.GetComponent<MeshRenderer>();
+                    if (renderer != null)
+                    {
+                        renderer.sortingLayerName = "Blocks";
+                        renderer.sortingOrder = 200;
+                    }
+
+                    Destroy(textObj, 2f);
+                }
 
                 cameraManager?.RegisterBlock(gameObject);
                 spawner?.SpawnBlockOnCrane();
